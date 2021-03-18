7a5773635dbc225fc17d7df42d53edb9a0af214a
nathan <1136026744@qq.com>
2021-03-18 14:55:20 +0800

    add demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 
 
 #include "aliases.h"
-#include "utils.h"
 
 using namespace org::nathan::aliases;
-using namespace org::nathan::utils;
 
 
-auto add(auto a, auto b) {
-    return a + b;
+#include <tuple>
+
+
+std::tuple<int, double> returnTuple() // return a tuple that contains an int and a double
+{
+    return {5, 6.7};
 }
 
-
 int main() {
-    cout << add(2, 2.2) << endl;
-
+    auto[a, b]{returnTuple()}; // used structured binding declaration to put results of tuple in variables a and b
+    std::cout << a << ' ' << b << '\n';
 
     return 0;
 }
