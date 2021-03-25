4672fbe95d5b28287bde052d2d267978af6727d3
nathan <1136026744@qq.com>
2021-03-25 21:31:05 +0800

    add SieveOfAtkin

---
+++ src/Euler/tools.h
@@ -0,0 +1,13 @@
+//
+// Created by Nathan on 2021/3/25.
+//
+
+#ifndef LEARNCPP_TOOLS_H
+#define LEARNCPP_TOOLS_H
+
+namespace org::nathan::Euler
+{
+    std::vector<long long> SieveOfAtkin(int limit);
+}
+
+#endif //LEARNCPP_TOOLS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 
+#include <iostream>
+#include <vector>
+#include "utils/utils.h"
+#include "Euler/tools.h"
+
+using std::cout, std::endl;
+
+using namespace org::nathan::utils;
+
+using namespace org::nathan::Euler;
 
 
 int main()
 {
-
+    auto t1 = currentTime();
+    int limit = 10000;
+    auto primes = SieveOfAtkin(limit);
+    long long res = 600851475143L;
+    for (long prime : primes)
+    {
+        while (res % prime == 0)
+        {
+            res /= prime;
+            if (res == 1)
+            {
+                cout << prime << endl;
+                break;
+            }
+        }
+    }
+    cout << res << endl;
+    auto t2 = currentTime();
+    cout << intervalToMilli(t2, t1) << endl;
 
     return 0;
 }
