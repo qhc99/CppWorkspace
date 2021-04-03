977673629161e5393a39abc65d658d65fa9e8c6b
nathan <1136026744@qq.com>
2021-04-03 20:19:22 +0800

    change README

--- src/org/nathan/learncpp/src/main.cpp
+++ src/org/nathan/learncpp/src/main.cpp
@@ -4,29 +4,20 @@
 #include <string>
 #include "euler/tools.h"
 #include "utils/utils.h"
-#include <string>
 
 using std::cout, std::endl, std::cin, std::string;
 
 using org::nathan::utils::currentTime, org::nathan::utils::intervalToMilli;
 
+using org::nathan::Euler::sieveOfEratosthenes;
+
 
 int main()
 {
     auto t1 = currentTime();
-    std::vector<int> vec(3, 100);
-
-    auto it = vec.begin();
-    it = vec.insert(it, 200);
-
-    vec.insert(it, 2, 300);
-
-    // "it" no longer valid, get a new one:
-    it = vec.begin();
-
-    std::vector<int> vec2(2, 400);
-    vec.insert(it + 2, vec2.begin(), vec2.end());
+    sieveOfEratosthenes(100000);
     auto t2 = currentTime();
+
     cout << intervalToMilli(t2, t1) << endl;
 
 
