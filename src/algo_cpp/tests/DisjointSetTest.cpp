81150e666390d325ea6391ff7fa34b70b34975bb
nathan <1136026744@qq.com>
2021-04-15 19:53:27 +0800

    migrate to boost test

--- src/algo_cpp/tests/DisjointSetTest.cpp
+++ src/algo_cpp/tests/DisjointSetTest.cpp
@@ -2,32 +2,25 @@
 // Created by Nathan on 2021/4/4.
 //
 
-#include <gtest/gtest.h>
+//#include <gtest/gtest.h>
 #include "algo_cpp/structures/DisjointSet.h"
+
+#define BOOST_TEST_MODULE DisjonintSetTest
+
+#include <boost/test/unit_test.hpp>
 
 using org::nathan::algo_cpp::structures::DisjointSet;
 
-class DisjointSetFixture : public ::testing::Test
-{
-};
+BOOST_AUTO_TEST_SUITE(DisjointSetAPITest) // NOLINT
 
-TEST_F(DisjointSetFixture, findSet) // NOLINT
-{
-    DisjointSet p{};
-    DisjointSet a{};
-    a.unionSet(p);
-    DisjointSet b{};
-    b.unionSet(p);
-    EXPECT_TRUE(&a.findGroupRep() == &b.findGroupRep()) << "findGroupRep()";
-}
+    BOOST_AUTO_TEST_CASE(findSet) // NOLINT
+    {
+        DisjointSet p{};
+        DisjointSet a{};
+        a.unionSet(p);
+        DisjointSet b{};
+        b.unionSet(p);
+        BOOST_CHECK_EQUAL(&a.findGroupRep(), &b.findGroupRep());
+    }
 
-TEST_F(DisjointSetFixture, foo) // NOLINT
-{
-    EXPECT_EQ(1, 1) << "foo";
-}
-
-//int main(int argc, char **argv)
-//{
-//    ::testing::InitGoogleTest(&argc, argv);
-//    return RUN_ALL_TESTS();
-//}
+BOOST_AUTO_TEST_SUITE_END() // NOLINT
--- src/algo_cpp/tests/RankSearchTest.cpp
+++ src/algo_cpp/tests/RankSearchTest.cpp
@@ -4,26 +4,26 @@
 
 #include "algo_cpp/miscellaneous/rank_search.hpp"
 #include "utils/utils.h"
-#include <gtest/gtest.h>
 #include <algorithm>
 
+#define BOOST_TEST_MODULE RankSearchTest
+
+#include <boost/test/unit_test.hpp>
 
 using org::nathan::algo_cpp::miscellaneous::RankSearch;
 using org::nathan::utils::shuffledRange;
 
-TEST(rankSearch, test)// NOLINT
-{
-    for (int i = 0; i < 100; i++)
-    {
-        std::vector<int> data{std::move(shuffledRange(1, 20))};
-        int r{RankSearch::find(data, 10)};
-        std::sort(data.begin(), data.end());
-        EXPECT_EQ(data[10], r);
-    }
-}
+BOOST_AUTO_TEST_SUITE(RankSearchAPITest) // NOLINT
 
-int main(int argc, char **argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+    BOOST_AUTO_TEST_CASE(findSet) // NOLINT
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            std::vector<int> data{std::move(shuffledRange(1, 20))};
+            int r{RankSearch::find(data, 10)};
+            std::sort(data.begin(), data.end());
+            BOOST_CHECK_EQUAL(data[10], r);
+        }
+    }
+
+BOOST_AUTO_TEST_SUITE_END() // NOLINT
