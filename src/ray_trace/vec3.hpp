e122a758cadae6ce7373a51ce21cdcc60ef120e1
qhc99 <onlinecqh99@gmail.com>
2022-12-05 20:17:43 -0500

    add ray tracing

---
+++ src/ray_trace/vec3.hpp
@@ -0,0 +1,61 @@
+//
+// Created by Nathan on 2022-12-05.
+//
+
+#ifndef DEV_QHC_CPP_PROJECTS_VEC3_HPP
+#define DEV_QHC_CPP_PROJECTS_VEC3_HPP
+
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+public:
+    vec3() : e{0,0,0} {}
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+
+    [[nodiscard]] double x() const { return e[0]; }
+    [[nodiscard]] double y() const { return e[1]; }
+    [[nodiscard]] double z() const { return e[2]; }
+
+    vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+    double operator[](int i) const { return e[i]; }
+    double& operator[](int i) { return e[i]; }
+
+    vec3& operator+=(const vec3 &v) {
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    vec3& operator*=(const double t) {
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3& operator/=(const double t) {
+        return *this *= 1/t;
+    }
+
+    [[nodiscard]] double length() const {
+        return sqrt(length_squared());
+    }
+
+    [[nodiscard]] double length_squared() const {
+        return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
+    }
+
+public:
+    double e[3];
+};
+
+// Type aliases for vec3
+using point3 = vec3;   // 3D point
+using color = vec3;    // RGB color
+
+#endif //DEV_QHC_CPP_PROJECTS_VEC3_HPP
