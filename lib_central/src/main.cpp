a73d3a4dd22a0593bc8aec7a74bdb9477c48ac61
qhc <onlinecqh99@gmail.com>
2022-12-11 17:45:56 -0500

    mat mul par

--- lib_central/src/main.cpp
+++ lib_central/src/main.cpp
@@ -6,24 +6,24 @@
 #include "lib_central/utils.h"
 #include <chrono>
 #include <cassert>
+#include <execution>
 
 using dev::qhc::utils::default_rand_engine;
 using std::chrono::duration_cast, std::chrono::nanoseconds;
 using dev::qhc::utils::current_time_point;
 using dev::qhc::utils::time_point_duration_to_ms;
 
-int main(){
-    assert(CHAR_BIT * sizeof (float) == 32);
+int main() {
     constexpr int size = 1300;
-    float **m1 = new float*[size];
-    float **m2 = new float*[size];
-    float **m3 = new float*[size];
+    float **m1 = new float *[size];
+    float **m2 = new float *[size];
+    float **m3 = new float *[size];
     std::uniform_real_distribution<float> dist(0, 1);
     auto rand_engine{default_rand_engine()};
     for (int i = 0; i < size; i++) {
-        m1[i] = new float [size];
-        m2[i] = new float [size];
-        m3[i] = new float [size];
+        m1[i] = new float[size];
+        m2[i] = new float[size];
+        m3[i] = new float[size];
     }
 
     for (int i = 0; i < size; i++) {
@@ -33,15 +33,25 @@ int main(){
             m3[i][j] = 0;
         }
     }
-    auto t1{current_time_point()};
+    std::cout << "start" << std::endl;
+    auto &&t1{current_time_point()};
+    std::vector<int> loop(size);
     for (int i = 0; i < size; i++) {
-        for(int j = 0; j < size; j++){
-            for (int k = 0; k < size; k++) {
-                m3[i][j] += m1[i][k] * m2[k][j];
-            }
-        }
+        loop.at(i) = i;
     }
-    auto t2{current_time_point()};
+
+    std::for_each(
+        std::execution::par,
+        loop.begin(),
+        loop.end(),
+        [&](auto &&i) {
+            for (int j = 0; j < size; j++) {
+                for (int k = 0; k < size; k++) {
+                    m3[i][j] += m1[i][k] * m2[k][j];
+                }
+            }
+        });
+    auto &&t2{current_time_point()};
     double ans = 0;
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
