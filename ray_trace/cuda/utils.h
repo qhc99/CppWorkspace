6a598d3abf2e2f56d2a521dc70189174924c8b78
qhc99 <onlinecqh99@gmail.com>
2023-01-06 16:02:39 -0500

    save

--- ray_trace/cuda/utils.h
+++ ray_trace/cuda/utils.h
@@ -22,7 +22,7 @@ static void HandleError( cudaError_t err,
                          const char *file,
                          int line ) {
     if (err != cudaSuccess) {
-        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+        fprintf( stderr,"%s in %s at line %d\n", cudaGetErrorString( err ),
                 file, line );
         exit( EXIT_FAILURE );
     }
