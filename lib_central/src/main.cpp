b0963ef1fd74ee2284af55f409ed398bd60510bd
qhc99 <onlinecqh99@gmail.com>
2023-01-14 16:00:59 -0500

    save

---
+++ lib_central/src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Nathan on 2023-01-14.
+//
+#include "lib_central/proto.h"
+
+int main(){
+
+}
---
+++ lib_central/src/public/lib_central/proto.h
@@ -0,0 +1,20 @@
+//
+// Created by Nathan on 2023-01-14.
+//
+
+#ifndef DEV_QHC_CPP_PROJECTS_PROTO_H
+#define DEV_QHC_CPP_PROJECTS_PROTO_H
+
+/**
+ * TernaryTries
+ * dancing links, sudoku
+ * TreelikeArray
+ * SegmentTree
+ * DisjointSet
+ * KMP
+ * RB Tree template and iterator
+ * BST printer
+ *
+ */
+
+#endif //DEV_QHC_CPP_PROJECTS_PROTO_H
