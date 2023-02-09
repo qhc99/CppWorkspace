b67929083021789d11ea57c0b2a30e5c637a5fb8
qhc99 <onlinecqh99@gmail.com>
2023-02-08 21:01:11 -0500

    add commented java code

--- lisp/Interpreter.cpp
+++ lisp/Interpreter.cpp
@@ -16,17 +16,105 @@ using std::string;
 shared_ptr<Value> Interpreter::let(shared_ptr<Pair> args,
                                    Interpreter &interpreter) {
   // TODO unfinished
+  /*
+  List<Object> x = treeList(_let);
+        x.add(args);
+        require(x, x.size() > 1);
+        List<List<Object>> bindings;
+        try {
+            bindings = (List<List<Object>>) args.get(0);
+        } catch (ClassCastException e) {
+            throw new ClassCastException("illegal binding list");
+        }
+        List<Object> body = args.subList(1, args.size());
+        require(x, bindings.stream().allMatch(b -> b != null &&
+                b.size() == 2 &&
+                b.get(0) instanceof Symbol), "illegal binding list");
+        List<Object> vars = bindings.stream().map(l -> l.get(0)).
+            collect(Collectors.toList());
+        List<Object> vals = bindings.stream().map(l -> l.get(1)).toList();
+        var t = treeList(_lambda, vars);
+        t.addAll(body.stream().map(interpreter::expand).toList());
+        var r = treeList(t);
+        r.addAll(vals.stream().map(interpreter::expand).toList());
+        return r;
+  */
   return nullptr;
 }
 
 shared_ptr<Value> Interpreter::parse(shared_ptr<Value> source,
                                      Interpreter &interpreter) {
   // TODO unfinished
+  /*
+  if (in instanceof String strIn) {
+            var t = read(new InputPort(strIn));
+            return interpreter.expand(t, true);
+        } else if (in instanceof InputPort inPort) {
+            var t = read(inPort);
+            return interpreter.expand(t, true);
+        } else {
+            throw new RuntimeException();
+        }
+  */
   return std::make_shared<Pair>(nullptr, nullptr);
 }
 
 shared_ptr<Value> Interpreter::eval(shared_ptr<Value> x, shared_ptr<Env> env) {
   // TODO unfinished
+  /*
+  while (true) {
+            if (x instanceof Symbol) {
+                return env.findEnv(x).get(x);
+            } else if (!(x instanceof List<?>)) {
+                return x;
+            }
+            List<Object> l = (List<Object>) x;
+            var op = l.get(0);
+            if (op.equals(_quote)) {
+                return l.get(1); // TODO return cons list
+            } else if (op.equals(_if)) {
+                var test = l.get(1);
+                var conseq = l.get(2);
+                var alt = l.get(3);
+                boolean testBool = isTrue(eval(test, env));
+                if (testBool) {
+                    x = conseq;
+                } else {
+                    x = alt;
+                }
+            } else if (op.equals(_set)) {
+                var v = l.get(1);
+                var exp = l.get(2);
+                env.findEnv(v).put(v, eval(exp, env));
+                return null;
+            } else if (op.equals(_define)) {
+                var v = l.get(1);
+                var exp = l.get(2);
+                env.put(v, eval(exp, env));
+                return null;
+            } else if (op.equals(_lambda)) {
+                var vars = l.get(1);
+                var exp = l.get(2);
+                return Procedure.newProcedure(vars, exp, env);
+            } else if (op.equals(_begin)) {
+                for (var exp : l.subList(1, l.size() - 1)) {
+                    eval(exp, env);
+                }
+                x = l.get(l.size() - 1);
+            } else {
+                Environment finalEnv = env;
+                List<Object> exps = l.stream().map(exp ->
+                    eval(exp, finalEnv)).collect(Collectors.toList());
+                var proc = exps.get(0);
+                exps = exps.subList(1, exps.size());
+                if (proc instanceof Procedure p) {
+                    x = p.expression();
+                    env = new Environment(p.parameters(), exps,
+  p.environment()); } else { return ((Lambda) proc).apply(exps);
+                }
+            }
+        }
+  */
   return nullptr;
 }
 
@@ -52,7 +140,7 @@ void Interpreter::repl() {
           std::cout << val->to_string() << std::flush;
         }
       }
-    } catch (BaseException &e) {
+    } catch (const BaseException &e) {
       std::cout << e.what() << std::endl << std::flush;
     }
   }
@@ -60,6 +148,23 @@ void Interpreter::repl() {
 
 void Interpreter::run_file(std::istream file) {
   // TODO unfinished
+  auto in_port{make_shared<InputPort>(file)};
+  while (true) {
+    try {
+      auto syms{parse(in_port)};
+      if (typeid(*syms) == typeid(Symbol) &&
+          *std::dynamic_pointer_cast<Symbol>(syms) == SYMBOLS::EOF_SYM) {
+        std::cout << std::flush;
+        return;
+      }
+      auto val{eval(syms, this->global_env)};
+      if (val != nullptr) {
+        std::cout << val->to_string();
+      }
+    } catch (const BaseException &e) {
+      std::cout << e.what() << std::endl;
+    }
+  }
 }
 
 shared_ptr<Value> Interpreter::eval_script() {
