270b5d83466c869c30022f915859de474b1a4669
qhc <onlinecqh99@gmail.com>
2024-05-26 14:57:53 -0400

    Fix env var config

--- central_lib/src/main.cpp
+++ central_lib/src/main.cpp
@@ -1,9 +1,13 @@
 //
 // Created by QC on 2023-01-14.
 //
-
+#include <vector>
+#include <iostream>
 int main()
 {
-
+    int t{2};
+    std::vector<int> v{1,2,3};
+    v[t*2] = 5;
+    std::cout << "out";
     return 0;
 }
