5d1c06df9f076634e233d3b419402a79ad1c43b6
qhc <onlinecqh99@gmail.com>
2024-08-03 23:06:46 -0400

    Update param

--- ray_trace/cuda/main.cu
+++ ray_trace/cuda/main.cu
@@ -146,9 +146,9 @@ __global__ void free_world(hitable **d_list, hitable **d_world, camera **d_camer
 }
 
 int main() {
-    int nx = 1200;
-    int ny = 800;
-    int ns = 500;
+    int nx = 600;
+    int ny = 400;
+    int ns = 100;
     int tx = 8;
     int ty = 8;
 
