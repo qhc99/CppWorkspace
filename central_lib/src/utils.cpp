feb088240d624767e7de319b65e561be0b6b097b
qhc99 <onlinecqh99@gmail.com>
2024-08-10 00:54:03 -0400

    Add pragma compiler check

--- central_lib/src/utils.cpp
+++ central_lib/src/utils.cpp
@@ -22,10 +22,14 @@ void leak()
 void out_of_range_access()
 {
     int* t = new int[2];
+#ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 6200 6386)
+#endif
     t[2] = 2; // NOLINT cppcoreguidelines-pro-bounds-pointer-arithmetic
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
 
     delete[] t;
 }
@@ -34,9 +38,13 @@ void use_after_free()
 {
     int* t = new int[2];
     delete[] t;
+#ifdef _MSC_VER
 #pragma warning(push)
 #pragma warning(disable : 6001)
+#endif
     t[1] = 1; // NOLINT cppcoreguidelines-pro-bounds-pointer-arithmetic
+#ifdef _MSC_VER
 #pragma warning(pop)
+#endif
 }
 }
