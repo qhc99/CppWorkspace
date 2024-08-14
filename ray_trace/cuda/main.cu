112d73accb5d3b5c5d2cf694444cd4d68ced15c2
qhc99 <onlinecqh99@gmail.com>
2024-08-13 21:06:30 -0400

    Use get err str

--- ray_trace/cuda/main.cu
+++ ray_trace/cuda/main.cu
@@ -15,7 +15,7 @@
 
 void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
     if (result) {
-        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << " at " <<
+        std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << "; " <<  cudaGetErrorString(result) << " at " <<
             file << ":" << line << " '" << func << "' \n";
         // Make sure we call CUDA Device Reset before exiting
         cudaDeviceReset();
