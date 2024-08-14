9bdfd230394b07b58fc0537105a99c8f1bef26d0
qhc99 <onlinecqh99@gmail.com>
2024-08-13 23:43:13 -0400

    Refactor

--- _cmake_libs/SystemChecks.cmake
+++ _cmake_libs/SystemChecks.cmake
@@ -5,13 +5,14 @@ find_program(LLVM_COV_EXIST llvm-cov)
 find_program(NVCC_COMPILER_EXIST nvcc)
 
 if(CMAKE_SYSTEM_NAME MATCHES Windows)
+    find_program(CL_COMPILER_EXIST cl)
     file(GLOB MSVC_DIR "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/*")
     # Get the first (or the most recent) version directory
     list(SORT MSVC_DIR ORDER DESCENDING)
     list(GET MSVC_DIR 0 MSVC_VERSION_DIR)
     set(MSVC_BIN_DIR "${MSVC_VERSION_DIR}/bin/Hostx64/x64")
     
-    if(NOT EXISTS "${MSVC_BIN_DIR}")
+    if((NOT EXISTS "${MSVC_BIN_DIR}") AND (NOT CL_COMPILER_EXIS))
         message(FATAL_ERROR "MSVC bin directory not found: ${MSVC_BIN_DIR}")
     endif()
 endif()
