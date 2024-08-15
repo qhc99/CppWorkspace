2e42a12b4c55e5061bab7150b021ca7a17d62497
qhc99 <onlinecqh99@gmail.com>
2024-08-15 14:41:11 -0400

    Add tests

--- build-test-all.sh
+++ build-test-all.sh
@@ -1,9 +1,11 @@
 cmake --preset "Clang Release"
 cd _build
 cmake --build . --target all 
+ctest -V -E ^asan_.*
 cd ..
 
 cmake --preset "Clang Debug"
 cd _build_debug
-cmake --build . --target all 
+cmake --build . --target all
+ctest -V -E ^asan_.*
 cd ..
