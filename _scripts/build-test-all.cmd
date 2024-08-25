6a99657a860214af65656bb9981204b647954b73
qhc99 <onlinecqh99@gmail.com>
2024-08-25 11:30:39 -0400

    Add clean

--- _scripts/build-test-all.cmd
+++ _scripts/build-test-all.cmd
@@ -1,17 +1,20 @@
 cmake --preset "Clang Release"
 cd _build
+cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
 
 cmake --preset "Clang Debug"
 cd _build_debug
+cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
 
 cmake --preset "VS 2022"
 cd _msbuild
+cmake --build . --target clean
 cmake --build . --target ALL_BUILD --config Release
 ctest -C Release -E ^asan_.* 
 cmake --build . --target ALL_BUILD --config Debug
