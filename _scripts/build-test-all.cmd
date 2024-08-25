abeb4abb3ab41810e2a0ca4688209904aee6b6d1
qhc99 <onlinecqh99@gmail.com>
2024-08-25 16:01:01 -0400

    Remove release test

--- _scripts/build-test-all.cmd
+++ _scripts/build-test-all.cmd
@@ -1,10 +1,3 @@
-cmake --preset "Clang Release"
-cd _build
-cmake --build . --target clean
-cmake --build . --target all
-ctest -E ^asan_.*
-cd ..
-
 cmake --preset "Clang Debug"
 cd _build_debug
 cmake --build . --target clean
@@ -15,8 +8,6 @@ cd ..
 cmake --preset "VS 2022"
 cd _msbuild
 cmake --build . --target clean
-cmake --build . --target ALL_BUILD --config Release
-ctest -C Release -E ^asan_.* 
 cmake --build . --target ALL_BUILD --config Debug
 ctest -C Debug -E ^asan_.* 
 cd ..
--- _scripts/build-test-all.sh
+++ _scripts/build-test-all.sh
@@ -1,10 +1,3 @@
-cmake --preset "Clang Release"
-cd _build
-cmake --build . --target clean
-cmake --build . --target all 
-ctest -E ^asan_.*
-cd ..
-
 cmake --preset "Clang Debug"
 cd _build_debug
 cmake --build . --target clean
