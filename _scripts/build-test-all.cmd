4c61a3797b73463861c108f01c917a19e212602a
qhc99 <onlinecqh99@gmail.com>
2024-08-25 17:28:53 -0400

    Remove clean

--- _scripts/build-test-all.cmd
+++ _scripts/build-test-all.cmd
@@ -1,13 +1,11 @@
 cmake --preset "Clang Debug"
 cd _build_debug
-cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
 
 cmake --preset "VS 2022"
 cd _msbuild
-cmake --build . --target clean
 cmake --build . --target ALL_BUILD --config Debug
 ctest -C Debug -E ^asan_.* 
 cd ..
--- _scripts/build-test-all.sh
+++ _scripts/build-test-all.sh
@@ -1,6 +1,5 @@
 cmake --preset "Clang Debug"
 cd _build_debug
-cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
