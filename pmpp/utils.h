8bd4751654b67559298d4e1b2340481fe22ffd73
qhc99 <onlinecqh99@gmail.com>
2024-08-24 23:42:09 -0400

    Refactor cuda utils

--- pmpp/utils.h
+++ pmpp/utils.h
@@ -2,18 +2,19 @@
 #define PMPP_EXAMPLE_UTILS
 
 #include <iostream>
+#include <cstdlib>
 #include <cuda_runtime_api.h>
 #include <device_launch_parameters.h>
 
-#define checkCudaErrors(val) check_cuda( (val), #val, __FILE__, __LINE__ )
+#define checkCudaErrors(val) check_cuda_error( (val), #val, __FILE__, __LINE__ )
 
-inline void check_cuda(cudaError_t result, char const *const func, const char *const file, int const line) {
+inline void check_cuda_error(cudaError_t result, char const *const func, const char *const file, int const line) {
     if (result != 0) {
         std::cerr << "CUDA error = " << static_cast<unsigned int>(result) << "; " <<  cudaGetErrorString(result) << " at " <<
             file << ":" << line << " '" << func << "' \n";
         // Make sure we call CUDA Device Reset before exiting
         cudaDeviceReset();
-        exit(99);
+        throw std::runtime_error("Exit");
     }
 }
 #endif
