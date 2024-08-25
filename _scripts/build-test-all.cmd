13e6a1f2aa15f7e847fc67afc47340d6d6364aeb
qhc99 <onlinecqh99@gmail.com>
2024-08-25 18:30:25 -0400

    Add date

--- _scripts/build-test-all.cmd
+++ _scripts/build-test-all.cmd
@@ -1,14 +1,19 @@
+date
 cmake --preset "Clang Release"
 cd _build
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
 
+date
+
 cmake --preset "Clang Debug"
 cd _build_debug
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
+
+date 
 
 cmake --preset "VS 2022"
 cd _msbuild
@@ -17,3 +22,5 @@ ctest -C Debug -E ^asan_.*
 cmake --build . --target ALL_BUILD --config Release
 ctest -C Debug -E ^asan_.*  
 cd ..
+
+date
--- _scripts/build-test-all.sh
+++ _scripts/build-test-all.sh
@@ -1,11 +1,19 @@
+date
+
 cmake --preset "Clang Release"
 cd _build
+cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
 
+date
+
 cmake --preset "Clang Debug"
 cd _build_debug
+cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
+
+date
