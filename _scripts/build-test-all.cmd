99d5a1aabd626a184c2ae0b0a251e0951b689636
qhc99 <onlinecqh99@gmail.com>
2024-08-25 20:17:50 -0400

    Add echo time

--- _scripts/build-test-all.cmd
+++ _scripts/build-test-all.cmd
@@ -1,29 +1,42 @@
-date
+@echo off
+echo The current time is: %time:~0,8%
+echo on
+
 cmake --preset "Clang Release"
 cd _build
+cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
 
-date
+@echo off
+echo The current time is: %time:~0,8%
+echo on
 
 cmake --preset "Clang Debug"
 cd _build_debug
+cmake --build . --target clean
 cmake --build . --target all
 ctest -E ^asan_.*
 cd ..
 
-date 
+@echo off
+echo The current time is: %time:~0,8%
+echo on 
 
 cmake --preset "VS 2022"
 cd _msbuild
 cmake --build . --target ALL_BUILD --config Debug
 ctest -C Debug -E ^asan_.*
 
-date 
+@echo off
+echo The current time is: %time:~0,8%
+echo on 
 
 cmake --build . --target ALL_BUILD --config Release
 ctest -C Debug -E ^asan_.*  
 cd ..
 
-date
+@echo off
+echo The current time is: %time:~0,8%
+echo on
