ed75d7c86b7ad447c0c3cc9b39f5060d5deb9230
qhc99 <onlinecqh99@gmail.com>
2024-08-26 21:02:54 -0400

    Add coarse factor

--- pmpp/mat_mul.cu
+++ pmpp/mat_mul.cu
@@ -1,6 +1,8 @@
 #include "utils.h"
+#include <algorithm>
+#include <cstddef>
 
-constexpr size_t TILE_WIDTH { 16 };
+constexpr size_t TILE_WIDTH { 32 };
 constexpr size_t COARSE_FACTOR { 4 };
 
 /**
@@ -26,22 +28,32 @@ __global__ void matMulKernel(float* A, float* B, float* C, size_t i, size_t j, s
     size_t ty { threadIdx.y };
 
     size_t row = bx * TILE_WIDTH + tx;
-    size_t col = by * TILE_WIDTH + ty;
+    size_t colStart = by * TILE_WIDTH + ty;
 
-    float p {};
+    float ps[COARSE_FACTOR];
+    for (size_t t {}; t < COARSE_FACTOR; ++t) {
+        ps[t] = 0;
+    }
 
     for (size_t ph {}; static_cast<float>(ph) < ceil(static_cast<float>(j) / static_cast<float>(TILE_WIDTH)); ph++) {
         Mds[tx * TILE_WIDTH + ty] = (row < i && (ph * TILE_WIDTH + ty) < j) ? A[row * j + ph * TILE_WIDTH + ty] : 0;
-        Nds[tx * TILE_WIDTH + ty] = ((ph * TILE_WIDTH + tx) < j && col < k) ? B[(ph * TILE_WIDTH + tx) * k + col] : 0;
-        __syncthreads();
 
-        for (size_t k {}; k < TILE_WIDTH; k++) {
-            p += Mds[tx * TILE_WIDTH + k] * Nds[k * TILE_WIDTH + ty];
+        for (size_t c {}; c < COARSE_FACTOR; ++c) {
+            size_t col { colStart + c * TILE_WIDTH };
+            Nds[tx * TILE_WIDTH + ty] = ((ph * TILE_WIDTH + tx) < j && col < k) ? B[(ph * TILE_WIDTH + tx) * k + col] : 0;
+            __syncthreads();
+
+            for (size_t k {}; k < TILE_WIDTH; k++) {
+                ps[c] += Mds[tx * TILE_WIDTH + k] * Nds[k * TILE_WIDTH + ty];
+            }
+            __syncthreads();
         }
-        __syncthreads();
     }
-    if (row < i && col < k) {
-        C[row * k + col] = p;
+    for (size_t c {}; c < COARSE_FACTOR; ++c) {
+        size_t col { colStart + c * TILE_WIDTH };
+        if (row < i && col < k) {
+            C[row * k + col] = ps[c];
+        }
     }
 }
 
