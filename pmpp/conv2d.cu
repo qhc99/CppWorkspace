0e8ef8ffde85d87c36d17eeb89687933ae44b34d
qhc99 <onlinecqh99@gmail.com>
2024-08-31 19:41:28 -0400

    Use macro

--- pmpp/conv2d.cu
+++ pmpp/conv2d.cu
@@ -1,6 +1,6 @@
-#define PPMP_CONV2D_MAX_RADIUS 6
+#define PPMP_CONV2D_FILTER_MAX_RADIUS 6
 
-__constant__ float filter[PPMP_CONV2D_MAX_RADIUS * 2 + 1];
+__constant__ float filter[PPMP_CONV2D_FILTER_MAX_RADIUS * 2 + 1];
 
 void conv2d(float *N, float *F, float *P, size_t radius, size_t width, size_t height){
 
--- pmpp/mat_mul.cu
+++ pmpp/mat_mul.cu
@@ -1,43 +1,48 @@
 #include "utils.h"
 
-constexpr size_t TILE_WIDTH { 16 };
-constexpr size_t COARSE_FACTOR { 2 };
+#ifndef PMPP_MAT_MUL_KERNEL_TILE_WIDTH
+#define PMPP_MAT_MUL_KERNEL_TILE_WIDTH 16
+#endif
+
+#ifndef PMPP_MAT_MUL_KERNEL_COARSE_FACTOR
+#define PMPP_MAT_MUL_KERNEL_COARSE_FACTOR 2
+#endif
 
 __global__ void matMulKernel(float* A, float* B, float* C, size_t i, size_t j, size_t k)
 {
     extern __shared__ float shared_mem[];
     float* Mds { shared_mem };
-    float* Nds { shared_mem + TILE_WIDTH * TILE_WIDTH };
+    float* Nds { shared_mem + PMPP_MAT_MUL_KERNEL_TILE_WIDTH * PMPP_MAT_MUL_KERNEL_TILE_WIDTH };
 
     size_t bx { blockIdx.x };
     size_t by { blockIdx.y };
     size_t tx { threadIdx.x };
     size_t ty { threadIdx.y };
 
-    size_t row = bx * TILE_WIDTH + tx;
-    size_t colStart = by * TILE_WIDTH + ty;
+    size_t row = bx * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + tx;
+    size_t colStart = by * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + ty;
 
-    float ps[COARSE_FACTOR];
-    for (size_t t {}; t < COARSE_FACTOR; ++t) {
+    float ps[PMPP_MAT_MUL_KERNEL_COARSE_FACTOR];
+    for (size_t t {}; t < PMPP_MAT_MUL_KERNEL_COARSE_FACTOR; ++t) {
         ps[t] = 0;
     }
 
-    for (size_t ph {}; static_cast<float>(ph) < ceil(static_cast<float>(j) / static_cast<float>(TILE_WIDTH)); ph++) {
-        Mds[tx * TILE_WIDTH + ty] = (row < i && (ph * TILE_WIDTH + ty) < j) ? A[row * j + ph * TILE_WIDTH + ty] : 0;
+    for (size_t ph {}; static_cast<float>(ph) < ceil(static_cast<float>(j) / static_cast<float>(PMPP_MAT_MUL_KERNEL_TILE_WIDTH)); ph++) {
+        Mds[tx * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + ty] = (row < i && (ph * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + ty) < j) ? A[row * j + ph * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + ty] : 0;
 
-        for (size_t c {}; c < COARSE_FACTOR; ++c) {
-            size_t col { colStart + c * TILE_WIDTH };
-            Nds[tx * TILE_WIDTH + ty] = ((ph * TILE_WIDTH + tx) < j && col < k) ? B[(ph * TILE_WIDTH + tx) * k + col] : 0;
+        for (size_t c {}; c < PMPP_MAT_MUL_KERNEL_COARSE_FACTOR; ++c) {
+            size_t col { colStart + c * PMPP_MAT_MUL_KERNEL_TILE_WIDTH };
+            Nds[tx * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + ty] = ((ph * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + tx) < j && col < k) ? B[(ph * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + tx) * k + col] : 0;
             __syncthreads();
 
-            for (size_t k {}; k < TILE_WIDTH; k++) {
-                ps[c] += Mds[tx * TILE_WIDTH + k] * Nds[k * TILE_WIDTH + ty];
+            for (size_t k {}; k < PMPP_MAT_MUL_KERNEL_TILE_WIDTH; k++) {
+                ps[c] += Mds[tx * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + k] * Nds[k * PMPP_MAT_MUL_KERNEL_TILE_WIDTH + ty];
             }
             __syncthreads();
         }
     }
-    for (size_t c {}; c < COARSE_FACTOR; ++c) {
-        size_t col { colStart + c * TILE_WIDTH };
+    for (size_t c {}; c < PMPP_MAT_MUL_KERNEL_COARSE_FACTOR; ++c) {
+        size_t col { colStart + c * PMPP_MAT_MUL_KERNEL_TILE_WIDTH };
         if (row < i && col < k) {
             C[row * k + col] = ps[c];
         }
@@ -57,11 +62,11 @@ void matMul(float* A, float* B, float* C, size_t i, size_t j, size_t k)
     checkCudaError(cudaMemcpy(A_d, A, i * j * sizeof(float), cudaMemcpyHostToDevice));
     checkCudaError(cudaMemcpy(B_d, B, j * k * sizeof(float), cudaMemcpyHostToDevice));
 
-    dim3 block_dim { TILE_WIDTH, TILE_WIDTH, 1 };
-    auto grid_dim_x { static_cast<unsigned int>(std::ceil(static_cast<float>(i) / static_cast<float>(TILE_WIDTH))) };
-    auto grid_dim_y { static_cast<unsigned int>(std::ceil(static_cast<float>(k) / static_cast<float>(TILE_WIDTH))) };
+    dim3 block_dim { PMPP_MAT_MUL_KERNEL_TILE_WIDTH, PMPP_MAT_MUL_KERNEL_TILE_WIDTH, 1 };
+    auto grid_dim_x { static_cast<unsigned int>(std::ceil(static_cast<float>(i) / static_cast<float>(PMPP_MAT_MUL_KERNEL_TILE_WIDTH))) };
+    auto grid_dim_y { static_cast<unsigned int>(std::ceil(static_cast<float>(k) / static_cast<float>(PMPP_MAT_MUL_KERNEL_TILE_WIDTH))) };
     dim3 grid_dim { grid_dim_x, grid_dim_y, 1 };
-    matMulKernel<<<grid_dim, block_dim, 2 * TILE_WIDTH * TILE_WIDTH * sizeof(float)>>>(A_d, B_d, C_d, i, j, k);
+    matMulKernel<<<grid_dim, block_dim, 2 * PMPP_MAT_MUL_KERNEL_TILE_WIDTH * PMPP_MAT_MUL_KERNEL_TILE_WIDTH * sizeof(float)>>>(A_d, B_d, C_d, i, j, k);
     checkCudaLastError();
     checkCudaError(cudaMemcpy(C, C_d, i * k * sizeof(float), cudaMemcpyDeviceToHost));
     checkCudaError(cudaFree(A_d));
