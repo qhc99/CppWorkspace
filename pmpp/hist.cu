190b4ecf0187b64a86fd217c0e63ad1f659b2516
qhc99 <onlinecqh99@gmail.com>
2024-09-08 20:29:56 -0400

    Add hist agg

--- pmpp/hist.cu
+++ pmpp/hist.cu
@@ -4,7 +4,7 @@
 #include "utils.h"
 // NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables,cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-avoid-c-arrays,cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays,cppcoreguidelines-pro-bounds-pointer-arithmetic,hicpp-no-array-decay,cppcoreguidelines-pro-bounds-constant-array-index)
 
-__global__ void histKernel(const char* data, unsigned int length, unsigned int* hist_out, unsigned int bin_width)
+__global__ void histKernel(const char* data, unsigned int length, unsigned int* hist_out, unsigned int bin_width, bool use_aggregation)
 {
     extern __shared__ unsigned int hist_shared[]; // bin_count * sizeof(unsigned int)
     const unsigned int bin_count = ceil(26. / bin_width);
@@ -12,11 +12,35 @@ __global__ void histKernel(const char* data, unsigned int length, unsigned int* 
         hist_shared[bin_idx] = 0U;
     }
     __syncthreads();
-    unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    for (auto i { tid }; i < length; i += blockDim.x * gridDim.x) {
-        int c { data[i] - 'a' };
-        if (c >= 0 && c < 26) {
-            atomicAdd(&(hist_shared[c / bin_width]), 1);
+    if (!use_aggregation) {
+        unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+        for (auto i { tid }; i < length; i += blockDim.x * gridDim.x) {
+            int c { data[i] - 'a' };
+            if (c >= 0 && c < 26) {
+                atomicAdd(&(hist_shared[c / bin_width]), 1);
+            }
+        }
+    } else {
+        unsigned int acc {};
+        int prev_bin_idx { -1 };
+        unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+        for (auto i { tid }; i < length; i += blockDim.x * gridDim.x) {
+            int c { data[i] - 'a' };
+            if (c >= 0 && c < 26) {
+                int bin_idx = c / static_cast<int>(bin_width);
+                if (bin_idx == prev_bin_idx) {
+                    ++acc;
+                } else {
+                    if (acc > 0) {
+                        atomicAdd(&(hist_shared[prev_bin_idx]), acc);
+                    }
+                    acc = 1;
+                    prev_bin_idx = bin_idx;
+                }
+            }
+        }
+        if (acc > 0) {
+            atomicAdd(&(hist_shared[prev_bin_idx]), acc);
         }
     }
     __syncthreads();
@@ -32,14 +56,13 @@ void hist(char* data, unsigned int length, unsigned int* hist_out, unsigned int 
 {
     char* data_d = nullptr;
     unsigned int* hist_d = nullptr;
-    const unsigned int bin_count = ceil(26./bin_width);
+    const unsigned int bin_count = ceil(26. / bin_width);
     checkCudaError(cudaMalloc(&data_d, length * sizeof(char)));
     checkCudaError(cudaMalloc(&hist_d, bin_count * sizeof(unsigned int)));
     checkCudaError(cudaMemcpy(data_d, data, length * sizeof(char), cudaMemcpyHostToDevice));
 
-    if (!use_aggregation) {
-        histKernel<<<4, 256, bin_count * sizeof(unsigned int)>>>(data_d, length, hist_d, bin_width);
-    }
+    histKernel<<<4, 256, bin_count * sizeof(unsigned int)>>>(data_d, length, hist_d, bin_width, use_aggregation);
+    
     checkCudaLastError();
 
     checkCudaError(cudaMemcpy(hist_out, hist_d, bin_count * sizeof(unsigned int), cudaMemcpyDeviceToHost));
--- pmpp/hist_test.cpp
+++ pmpp/hist_test.cpp
@@ -1,33 +1,57 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
 #include "hist.h"
+#include <doctest/doctest.h>
 
-void hist_test(unsigned int length, unsigned int *out, unsigned int bin_width=2)
+constexpr unsigned int bin_width { 2 };
+constexpr unsigned int bin_count { 13 };
+
+void hist_test(unsigned int length, unsigned int* out, bool agg)
 {
     auto data = std::make_unique<std::vector<char>>(length);
 #pragma omp parallel for
     for (unsigned int i {}; i < length; i++) {
         (*data)[i] = static_cast<char>(i % 26) + 'a';
     }
-    hist(data->data(), length, out, bin_width);
+    hist(data->data(), length, out, bin_width, agg);
 }
 
 TEST_CASE("hist_large_test")
 {
-    constexpr unsigned int length = 26*512;
-    auto out{std::make_unique<std::vector<unsigned int>>(13)};
-    hist_test(length,out->data());
-    for (unsigned int i {}; i < 13; i++) {
+    constexpr unsigned int length = 26 * 512;
+    auto out { std::make_unique<std::vector<unsigned int>>(bin_count) };
+    hist_test(length, out->data(), false);
+    for (unsigned int i {}; i < bin_count; i++) {
         REQUIRE_EQ(1024, (*out)[i]);
     }
 }
 
+TEST_CASE("hist_agg_large_test")
+{
+    constexpr unsigned int length = 26 * 512;
+    auto out2 { std::make_unique<std::vector<unsigned int>>(bin_count) };
+    hist_test(length, out2->data(), true);
+    for (unsigned int i {}; i < bin_count; i++) {
+        REQUIRE_EQ(1024, (*out2)[i]);
+    }
+}
+
+
 TEST_CASE("hist_small_test")
 {
     constexpr unsigned int length = 26;
-    auto out{std::make_unique<std::vector<unsigned int>>(13)};
-    hist_test(length,out->data());
-        for (unsigned int i {}; i < 13; i++) {
+    auto out { std::make_unique<std::vector<unsigned int>>(bin_count) };
+    hist_test(length, out->data(), false);
+    for (unsigned int i {}; i < bin_count; i++) {
         REQUIRE_EQ(2, (*out)[i]);
+    }
+}
+
+TEST_CASE("hist_agg_small_test")
+{
+    constexpr unsigned int length = 26;
+    auto out2 { std::make_unique<std::vector<unsigned int>>(bin_count) };
+    hist_test(length, out2->data(), true);
+    for (unsigned int i {}; i < bin_count; i++) {
+        REQUIRE_EQ(2, (*out2)[i]);
     }
 }
