52e5a0d15f5f5826d95c17b39f204d9d6610be75
qhc99 <onlinecqh99@gmail.com>
2024-09-09 20:12:23 -0400

    Format

--- x64_asm_linux/fact.asm
+++ x64_asm_linux/fact.asm
@@ -1,4 +1,4 @@
-; linux abi: 
+; linux abi:
 ; stack alignment: 2 bytes (1 word)
 ; float function args: xmm0-xmm7 (xmm0-xmm3 on windows)
 ; int function args: rdi, rsi , rdx, rcx, r8, r9, stack in reverse order
@@ -6,14 +6,13 @@
 ; variable number of args (e.g., printf): rax
 ; int return: rax
 ; float return: xmm0
-
     section .note.GNU-stack noalloc noexec nowrite progbits
+
     segment .data
     x dq 0
     scanf_format db "%ld", 0
     printf_format db "fact(%ld) = %ld", 0x0a, 0
     scanf_hint db "Input a number:", 0x0a, 0
-
     segment .text
     global main
     global fact
@@ -23,9 +22,9 @@
 main:
     push rbp
     mov rbp, rsp
-    lea rdi, [scanf_hint]  
-    xor eax, eax            
-    call printf            
+    lea rdi, [scanf_hint]
+    xor eax, eax
+    call printf
     lea rdi, [scanf_format] ; scanf arg1
     lea rsi, [x] ; scanf arg2
     xor eax, eax
@@ -51,7 +50,6 @@ fact:
     mov eax, 1 ; return value 1
     leave
     ret
-
 greater:
     mov [rsp+n], rdi ; save n
     dec rdi
--- x64_asm_linux/float_example.asm
+++ x64_asm_linux/float_example.asm
@@ -1,10 +1,49 @@
     section .note.GNU-stack noalloc noexec nowrite progbits
-    
-    segment .data
-    x dq 0
-    scanf_format db "%ld", 0
-    printf_format db "fact(%ld) = %ld", 0x0a, 0
-    scanf_hint db "Input a number:", 0x0a, 0
 
+    segment .data
+    point_format db "%f,%f,f%", 0x0a, 0
+    point_prompt db "Input 6 floats (two 3d points)", 0x0a, 0
+    dist_format db "distance = %f", 0x0a, 0
+    dotp_format db "dot product result: %f", 0x0a, 0
+    
     segment .text
     global main
+
+distance3d: ; (double* rdi, double* rsi)
+    movss xmm0, [rdi] ; x from first point
+    subss xmm0, [rsi] ; subtract x from second point
+    mulss xmm0, xmm0 ; (x1-x2)^2
+    movss xmm1, [rdi+4] ; y from first point
+    subss xmm1, [rsi+4] ; subtract y from second point
+    mulss xmm1, xmm1 ; (y1-y2)^2
+    movss xmm2, [rdi+8] ; z from first po int
+    subss xmm2, [rsi+8] ; subtract z from second point
+    mulss xmm2, xmm2 ; (z1-z2)^2
+    addss xmm0, xmm1 ; (x1-x2)^2 + (y1-y2)^2
+    addss xmm0, xmm2 ; (x1-x2)^2 + (y1-y2)^2 +  (y1-y2)^2
+    sqrtss xmm0, xmm0 ; sqrt sum
+    ret
+
+dot_product: ; (double* rdi, double* rsi)
+    movss xmm0, [rdi]
+    mulss xmm0, [rsi]
+    movss xmm1, [rdi+4]
+    mulss xmm1, [rsi+4]
+    addss xmm0, xmm1
+    movss xmm2, [rdi+8]
+    mulss xmm2, [rsi+8]
+    addss xmm0, xmm2
+    ret
+
+horner: ; (double coef *rdi, double value xmm0, int degree rsi)
+    movs xmm1, xmm0 ; xmm1 = value
+    movsv xmm0, [rdi+rsi*8] ; accumulator
+    cmp rsi, 0 ; degree = 0 ?
+    jz done
+more:
+    sub esi, 1
+    mulsd xmm0, xmm1 ; b_k*x
+    addsd xmm0, [rdi+rsi*8] ; acc + coef_k
+    jnz more
+done:
+    ret
