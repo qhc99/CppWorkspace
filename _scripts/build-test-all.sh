489094fbc8a2289bf89742542aba4765b964d9d0
qhc99 <onlinecqh99@gmail.com>
2024-09-16 00:50:33 -0400

    Update build scripts

--- _scripts/build-test-all.sh
+++ _scripts/build-test-all.sh
@@ -1,9 +1,9 @@
-date
-
 cmake --preset "Clang Debug"
 cd _build_debug
+
+echo "[$(date '+%H:%M:%S')]: Build start"
 cmake --build . --target all  --parallel
+echo "[$(date '+%H:%M:%S')]: Build end"
+
 ctest -E ^asan_.*
 cd ..
-
-date
