8ab8983c5c01db702753e5b114a3cdc189d2e240
qhc99 <onlinecqh99@gmail.com>
2024-09-18 00:43:14 -0400

    Add shared lib demo, fix opengl cmake

--- central_lib/src/public/lib_central/utils.h
+++ central_lib/src/public/lib_central/utils.h
@@ -1,6 +1,22 @@
 #ifndef DEV_QC_CENTRAL_LIB_UTILS_H
 #define DEV_QC_CENTRAL_LIB_UTILS_H
 
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+  #ifdef BUILDING_CENTRAL_LIB
+    #define CENTRAL_LIB_API __declspec(dllexport)
+  #else
+    #define CENTRAL_LIB_API __declspec(dllimport)
+  #endif
+#else
+  #ifdef BUILDING_CENTRAL_LIB
+    #define CENTRAL_LIB_API __attribute__((visibility("default")))
+  #else
+    #define CENTRAL_LIB_API
+  #endif
+#endif
+
+
 namespace dev::qhc::utils {
 
 using std::vector;
@@ -42,7 +58,7 @@ inline long long time_point_duration_to_us(steady_clock_time_point current,
  * @param high exclude
  * @return
  */
-auto shuffledRange(int low, int high) -> vector<int>;
+CENTRAL_LIB_API vector<int> shuffledRange(int low, int high);
 
 void leak();
 
