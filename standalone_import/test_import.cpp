301235d11e217b40f9f5dfb624c30eae5047a2ec
qhc99 <onlinecqh99@gmail.com>
2024-09-20 00:24:25 -0400

    Update test

--- standalone_import/test_import.cpp
+++ standalone_import/test_import.cpp
@@ -1,9 +1,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest/doctest.h"
 #include "lib_central/dll.h"
+#include <numeric>
 
 TEST_CASE("shared_lib_link_test")
 {
     auto v {dev::qhc::utils::shuffledRange(0, 6)};
-    REQUIRE(v[0] < 100);
+    auto res{std::accumulate(v.begin(), v.end(), 0)};
+    REQUIRE_EQ(15,res);
 }
