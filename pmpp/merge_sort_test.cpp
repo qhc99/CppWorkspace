a3fa1dc7b4c34614ff74cd0385f42cd66a4c9a22
qhc99 <onlinecqh99@gmail.com>
2024-09-26 20:23:47 -0400

    Fix merge sort test

--- pmpp/merge_sort_test.cpp
+++ pmpp/merge_sort_test.cpp
@@ -1,12 +1,42 @@
+#include <vector>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "merge_sort.h"
 #include <doctest/doctest.h>
 
 TEST_CASE("merge_sort_small_test")
 {
+    std::vector<int> a(100);
+    std::iota(a.begin(), a.end(), 0);
+    std::transform(a.begin(), a.end(), a.begin(), [](int x) { return x * 2; });
 
+    std::vector<int> b(100);
+    std::iota(b.begin(), b.end(), 0);
+    std::transform(b.begin(), b.end(), b.begin(), [](int x) { return x * 2 + 1; });
+
+    std::vector<int> c(a.size() + b.size());
+    mergeTiles(a.data(), a.size(), b.data(), b.size(), c.data());
+
+    for (size_t i {}; i < c.size(); i++) {
+        REQUIRE_EQ(i, c.at(i));
+    }
 }
 
 TEST_CASE("merge_sort_large_test")
 {
+    std::vector<int> a(2000);
+    std::iota(a.begin(), a.end(), 0);
+    std::transform(a.begin(), a.end(), a.begin(), [](int x) { return x * 2; });
 
+    std::vector<int> b(2000);
+    std::iota(b.begin(), b.end(), 0);
+    std::transform(b.begin(), b.end(), b.begin(), [](int x) { return x * 2 + 1; });
+
+    std::vector<int> c(a.size() + b.size());
+    mergeTiles(a.data(), a.size(), b.data(), b.size(), c.data());
+
+    for (size_t i { 1 }; i < c.size(); i++) {
+        if (i % 1024 != 0) {
+            REQUIRE(c.at(i) > c.at(i - 1));
+        }
+    }
 }
